6a409f7bea3cde380af7d446305e827905190577
Fuku1213 <fukufuku121303@gmail.com>
2022-12-14 23:59:07 +0900

    Quarog公開にかかる変更

--- js/download.js
+++ js/download.js
@@ -3,7 +3,7 @@ function withoutRuntime() {
         display: "block"
     });
 
-    $("#quarog-dl").attr("href", "#a"); // #a -> withoutRuntimeダウンロードリンク
+    $("#quarog-dl").attr("href", "https://drive.google.com/u/0/uc?id=1y1wmth7EADqgRqVV44pH_CwcG1iIVa59&export=download");
 }
 
 function mergedRuntime() {
@@ -11,7 +11,7 @@ function mergedRuntime() {
         display: "block"
     });
 
-    $("#quarog-dl").attr("href", "#b"); // #b -> mergedRuntimeダウンロードリンク
+    $("#quarog-dl").attr("href", "https://drive.google.com/u/0/uc?id=1dC-NtMg6ItHmerlmxBD83TjK4Qwh7Z6k&export=download");
 }
 
 function downloadQuarog() {
